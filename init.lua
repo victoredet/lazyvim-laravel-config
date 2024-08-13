e8faa197f20394dd21bbdc32063a919abcd6c318
Victor Edet <edetvictor849@gmail.com>
2024-08-13 00:20:46 +0000

    (chore): add flutter formatter

--- init.lua
+++ init.lua
@@ -1,2 +1,13 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+
+-- require("flutter-tools").setup({}) -- use defaults
 require("config.lazy")
+
+local Plug = vim.fn["plug#"]
+
+vim.call("plug#begin")
+
+Plug("tpope/vim-sensible")
+
+Plug("dart-lang/dart-vim-plugin")
+vim.call("plug#end")
